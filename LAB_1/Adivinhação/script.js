46cd9ebf3fd0a355b0daeb08e0922edefb1c6b03
lucaberribillibortoleto <lucabortoleto@gmail.com>
2025-03-25 16:36:35 -0300

    Aula_6

---
+++ LAB_1/Adivinhação/script.js
@@ -0,0 +1,7 @@
+function Adivinhação(){
+    let num1 = parseInt(document.getElementById().value);
+    let num2 = parseInt(document.getElementById().value);
+    let result = soma(num1,num2);
+    console.log(result);
+    document.getElementById("Adivinhação_resp").innerHTML = ;
+}
