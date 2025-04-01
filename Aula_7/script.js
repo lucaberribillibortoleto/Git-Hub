ddaf97c769c6eba3a48703d2d090fca6e7252d85
lucaberribillibortoleto <lucabortoleto@gmail.com>
2025-04-01 14:14:58 -0300

    Aula07

---
+++ Aula_7/script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.fillRect(10,10,50,50);
+ctx.strokeRect(10,10,50,50);
+ctx.closePath();
