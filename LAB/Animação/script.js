0ce6fa34a0a9bff39d4f68eae2fbb6591bdc04ef
lucaberribillibortoleto <lucabortoleto@gmail.com>
2025-04-15 14:57:44 -0300

    Aula_9

--- LAB/Animação/script.js
+++ LAB/Animação/script.js
@@ -1,43 +1,58 @@
-const canvas = document.getElementById('Canvas1');
-const ctx = canvas.getContext('2d');
+class Retangulo {
+  constructor(cor_linha, cor_preenchimento, espessura_linha, x, y, largura, altura) {
+    this.cor_linha = cor_linha;
+    this.cor_preenchimento = cor_preenchimento;
+    this.espessura_linha = espessura_linha;
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+  }
 
-
-const img = new Image();
-img.src = 'https://via.placeholder.com/50'; 
-
-
-const imgWidth = 50;
-const imgHeight = 50;
-
-
-let imgX = (canvas.width - imgWidth) / 2;
-let imgY = (canvas.height - imgHeight) / 2;
-
-
-function desenhar() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(img, imgX, imgY, imgWidth, imgHeight);
+  desenhe(contexto) {
+    contexto.beginPath();
+    contexto.lineWidth = this.espessura_linha;
+    contexto.fillStyle = this.cor_preenchimento;
+    contexto.strokeStyle = this.cor_linha;
+    contexto.fillRect(this.x, this.y, this.largura, this.altura);
+    contexto.strokeRect(this.x, this.y, this.largura, this.altura);
+    contexto.closePath();
+  }
 }
 
-document.addEventListener('mousemove', function(event) {
-  const rect = canvas.getBoundingClientRect();
-  const mouseX = event.clientX - rect.left;
-  const mouseY = event.clientY - rect.top;
+const canvas = document.getElementById('canvas');
+const ctx1 = canvas.getContext('2d');
 
-  imgX = Math.min(
-    Math.max(mouseX - imgWidth / 2, 0),
-    canvas.width - imgWidth
-  );
-  imgY = Math.min(
-    Math.max(mouseY - imgHeight / 2, 0),
-    canvas.height - imgHeight
-  );
+let retangulo_3 = new Retangulo('blue', 'yellow', 3, 200, 200, 20, 20);
 
-  desenhar();
+function animacao() {
+  ctx1.clearRect(0, 0, canvas.width, canvas.height);
+  retangulo_3.desenhe(ctx1);
+  requestAnimationFrame(animacao);
+}
+
+document.addEventListener('mousemove', function(evento) {
+  let rect = canvas.getBoundingClientRect();
+  let x_mouse = evento.clientX - rect.left;
+  let y_mouse = evento.clientY - rect.top;
+
+  
+  let novoX = x_mouse - retangulo_3.largura / 2;
+  let novoY = y_mouse - retangulo_3.altura / 2;
+
+  if (novoX < 0) novoX = 0;
+  if (novoX + retangulo_3.largura > canvas.width) {
+    novoX = canvas.width - retangulo_3.largura;
+  }
+
+  
+  if (novoY < 0) novoY = 0;
+  if (novoY + retangulo_3.altura > canvas.height) {
+    novoY = canvas.height - retangulo_3.altura;
+  }
+
+  retangulo_3.x = novoX;
+  retangulo_3.y = novoY;
 });
 
-canvas.addEventListener('mouseleave', () => {
-  desenhar(); 
-});
-
-img.onload = desenhar;
+animacao();
